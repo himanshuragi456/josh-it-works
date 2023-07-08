be14f03218f0280cc25f65229077bb9c650e6033
aditya-ragi <6265220110a@gmail.com>
2023-07-08 09:20:46 +0530

    web photos updated

--- src/parts/AllPortfolio.js
+++ src/parts/AllPortfolio.js
@@ -42,7 +42,7 @@ export default function AllPortfolio({ data }) {
                     <Button type="link" href={`/project/${item.id}`}>
                       <div className="group rounded-2xl shadow-xl w-auto w-11/12 m-3 transform transition duration-500 hover:scale-110 portofolio-card">
                         <div className="relative">
-                          <img src={item.imageUrl} alt="Portfolio" className="rounded-t-2xl z-0" />
+                          <img src={item.src} alt="Portfolio" className="rounded-t-2xl z-0" />
                           <div className="absolute flex w-full h-full top-0 opacity-0 bg-black justify-center rounded-t-2xl rounded-b img-hover">
                             <button className="focus:outline-none">
                               <svg className="w-20 h-20 text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -71,7 +71,7 @@ export default function AllPortfolio({ data }) {
                     <Button type="link" href={`/project/${item.id}`}>
                       <div className="group rounded-2xl shadow-xl w-auto w-11/12 m-3 transform transition duration-500 hover:scale-110 portofolio-card">
                         <div className="relative">
-                          <img src={item.imageUrl} alt="Portfolio" className="rounded-t-2xl z-0" />
+                          <img src={item.src} alt="Portfolio" className="rounded-t-2xl z-0" />
                           <div className="absolute flex w-full h-full top-0 opacity-0 bg-black justify-center rounded-t-2xl rounded-b img-hover">
                             <button className="focus:outline-none">
                               <svg className="w-20 h-20 text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -100,7 +100,7 @@ export default function AllPortfolio({ data }) {
                     <Button type="link" href={`/project/${item.id}`}>
                       <div className="group rounded-2xl shadow-xl w-auto w-11/12 m-3 transform transition duration-500 hover:scale-110 portofolio-card">
                         <div className="relative">
-                          <img src={item.imageUrl} alt="Portfolio" className="rounded-t-2xl z-0" />
+                          <img src={item.src} alt="Portfolio" className="rounded-t-2xl z-0" />
                           <div className="absolute flex w-full h-full top-0 opacity-0 bg-black justify-center rounded-t-2xl rounded-b img-hover">
                             <button className="focus:outline-none">
                               <svg className="w-20 h-20 text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
